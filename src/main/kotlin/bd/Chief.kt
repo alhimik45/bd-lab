0b61c405e86a60a327f376baa6677ebb03fa80f7
alhimik45 <mail@alhimik.me>
2017-12-13 15:07:45 +0700

    search

--- src/main/kotlin/bd/Chief.kt
+++ src/main/kotlin/bd/Chief.kt
@@ -3,6 +3,7 @@ package bd
 import javafx.scene.control.TabPane
 import javafx.scene.control.TableColumn
 import javafx.scene.control.TableView
+import javafx.scene.control.TextField
 import javafx.scene.control.cell.PropertyValueFactory
 import test.generated.Tables
 import test.generated.tables.pojos.AllDistr
@@ -19,6 +20,7 @@ class Chief : View("Начальника") {
 
     private val distribTable: TableView<AllDistr> by fxid()
     private val employeeTable: TableView<EmployeView> by fxid()
+    private val searchEmployee: TextField by fxid()
 
     private val fio: TableColumn<AllDistr, String> by fxid()
     private val fioM: TableColumn<EmployeView, String> by fxid()
@@ -39,6 +41,10 @@ class Chief : View("Начальника") {
         update()
         updateMan()
         EventBus.on(Events.EMP_UPD) { updateMan() }
+        searchEmployee.textProperty().addListener { _,_,_ ->
+            updateMan()
+        }
+
     }
 
     fun update() {
@@ -50,7 +56,19 @@ class Chief : View("Начальника") {
     fun updateMan() {
         val data = employeeTable.items
         data.clear()
-        data.addAll(Logic.create!!.select()?.from(Tables.EMPLOYE_VIEW)?.fetch()?.into(EmployeView::class.java)!!.asIterable())
+        data.addAll(Logic.create!!.select()?.from(Tables.EMPLOYE_VIEW)?.fetch()?.into(EmployeView::class.java)!!.filter { e ->
+            val v = searchEmployee.text
+            if (v == null || v.isEmpty()) {
+                true
+            } else {
+                val lowerCaseFilter = v.toLowerCase().split(" ").filter { it.isNotBlank() }
+                if (lowerCaseFilter.any{e.fio.toLowerCase().contains(it)}) {
+                    true
+                } else {
+                    lowerCaseFilter.any{e.personalid.toLowerCase().contains(it)}
+                }
+            }
+        })
     }
 
     fun newPid() {
