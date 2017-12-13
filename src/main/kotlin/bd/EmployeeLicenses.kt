70a8c6d846fceaec800e55662c953a99d6706ee5
Hvorov Kostya <hvorovk@gmail.com>
2017-12-13 12:23:56 +0700

    add search function

--- src/main/kotlin/bd/EmployeeLicenses.kt
+++ src/main/kotlin/bd/EmployeeLicenses.kt
@@ -3,6 +3,7 @@ package bd
 import javafx.scene.control.TabPane
 import javafx.scene.control.TableColumn
 import javafx.scene.control.TableView
+import javafx.scene.control.TextField
 import javafx.scene.control.cell.PropertyValueFactory
 import test.generated.tables.pojos.DriverLicenseView
 import test.generated.tables.pojos.ExamView
@@ -10,6 +11,9 @@ import test.generated.Tables.DRIVER_LICENSE_VIEW
 import test.generated.Tables.EXAM_VIEW
 import tornadofx.*
 import java.util.*
+import javafx.collections.transformation.FilteredList
+
+
 
 class EmployeeLicenses : View("Учет водительских удостоверений") {
     override val root: TabPane by fxml()
@@ -20,8 +24,14 @@ class EmployeeLicenses : View("Учет водительских удостов�
     private val date: TableColumn<DriverLicenseView, Date> by fxid()
     private val examTable: TableView<ExamView> by fxid()
     private val fio1: TableColumn<ExamView, String> by fxid()
-    private val ocenka: TableColumn<ExamView, String> by fxid()
+    private val result: TableColumn<ExamView, String> by fxid()
     private val date1: TableColumn<ExamView, Date> by fxid()
+    private val searchLicenses: TextField by fxid()
+    private val searchExam: TextField by fxid()
+    private val dataDrLi = licenseTable.items
+    private val dataEx = examTable.items
+    private var searchDrLi = FilteredList<DriverLicenseView>(dataDrLi){ _ -> true}
+    private var searchEx = FilteredList<ExamView>(examTable.items){ _ -> true}
 
     init {
         fio.cellValueFactory = PropertyValueFactory<DriverLicenseView, String>("fio")
@@ -29,21 +39,48 @@ class EmployeeLicenses : View("Учет водительских удостов�
         num.cellValueFactory = PropertyValueFactory<DriverLicenseView, String>("licenseid")
         date.cellValueFactory = PropertyValueFactory<DriverLicenseView, Date>("dateofissue")
         fio1.cellValueFactory = PropertyValueFactory<ExamView, String>("fio")
-        ocenka.cellValueFactory = PropertyValueFactory<ExamView, String>("results")
+        result.cellValueFactory = PropertyValueFactory<ExamView, String>("results")
         date1.cellValueFactory = PropertyValueFactory<ExamView, Date>("date")
         updateDrLi()
         updateEx()
+        searchLicenses.textProperty().addListener { _, _, newValue ->
+            searchDrLi.setPredicate { item ->
+                if (newValue == null || newValue.isEmpty()) {
+                    true
+                } else {
+                    val lowerCaseFilter = newValue.toLowerCase()
+
+                    if (item.fio.toLowerCase().contains(lowerCaseFilter)) {
+                        true
+                    } else {
+                        item.cat.toLowerCase().contains(lowerCaseFilter)
+                    }
+                }
+            }
+        }
+
+        searchExam.textProperty().addListener { _, _, newValue ->
+            searchEx.setPredicate { item ->
+                if (newValue == null || newValue.isEmpty()) {
+                    true
+                } else {
+                    item.fio.toLowerCase().contains(newValue.toLowerCase())
+                }
+            }
+        }
     }
 
     fun updateDrLi() {
-        val data = licenseTable.items
-        data.clear()
-        data.addAll(Logic.create!!.select()?.from(DRIVER_LICENSE_VIEW)?.fetch()?.into(DriverLicenseView::class.java)!!.asIterable())
+        dataDrLi.clear()
+        dataDrLi.addAll(Logic.create!!.select()?.from(DRIVER_LICENSE_VIEW)?.fetch()?.into(DriverLicenseView::class.java)!!.asIterable())
+        searchDrLi = FilteredList<DriverLicenseView>(dataDrLi){ _ -> true}
+        licenseTable.items = searchDrLi
     }
 
     fun updateEx() {
-        val data = examTable.items
-        data.clear()
-        data.addAll(Logic.create!!.select()?.from(EXAM_VIEW)?.fetch()?.into(ExamView::class.java)!!.asIterable())
+        dataEx.clear()
+        dataEx.addAll(Logic.create!!.select()?.from(EXAM_VIEW)?.fetch()?.into(ExamView::class.java)!!.asIterable())
+        searchEx = FilteredList<ExamView>(examTable.items){ _ -> true}
+        examTable.items = searchEx
     }
 }
